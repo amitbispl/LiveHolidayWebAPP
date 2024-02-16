9e4512ea1a35450124e737795915a409908ad122
Ajay singh <ajayshaktawat.bispl@gmail.com>
2024-02-16 19:45:12 +0530

    commit

---
+++ LiveHolidayapp/LiveHolidayapp/RenderViewExtension.cs
@@ -0,0 +1,207 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Reflection;
+
+namespace LiveHolidayapp
+{
+    public static class RenderViewExtension
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Join<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            if (first == null)
+            {
+                return second;
+            }
+            if (second == null)
+            {
+                return first;
+            }
+
+            return first.Concat(second).ToList();
+        }
+
+        private static readonly IDictionary<Type, ICollection<PropertyInfo>> _Properties =
+      new Dictionary<Type, ICollection<PropertyInfo>>();
+
+        public static DataTable ToDataTable<T>(this List<T> items)
+        {
+            var tb = new DataTable(typeof(T).Name);
+            PropertyInfo[] props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in props)
+            {
+                Type t = GetCoreType(prop.PropertyType);
+                tb.Columns.Add(prop.Name, t);
+            }
+            foreach (T item in items)
+            {
+                var values = new object[props.Length];
+
+                for (int i = 0; i < props.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item, null);
+                }
+
+                tb.Rows.Add(values);
+            }
+
+            return tb;
+        }
+
+        public static IEnumerable<T> DataTableToList<T>(this DataTable table) where T : class, new()
+        {
+            try
+            {
+                var objType = typeof(T);
+                ICollection<PropertyInfo> properties;
+
+                lock (_Properties)
+                {
+                    if (!_Properties.TryGetValue(objType, out properties))
+                    {
+                        properties = objType.GetProperties().Where(property => property.CanWrite).ToList();
+                        _Properties.Add(objType, properties);
+                    }
+                }
+
+                var list = new List<T>(table.Rows.Count);
+
+                foreach (var row in table.AsEnumerable())
+                {
+                    var obj = new T();
+
+                    foreach (var prop in properties)
+                    {
+                        try
+                        {
+                            var propType = Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType;
+                            var safeValue = row[prop.Name] == null ? null : Convert.ChangeType(row[prop.Name], propType);
+
+                            prop.SetValue(obj, safeValue, null);
+                        }
+                        catch
+                        {
+                            // ignored
+                        }
+                    }
+
+                    list.Add(obj);
+                }
+
+                return list;
+            }
+            catch
+            {
+                return Enumerable.Empty<T>();
+            }
+        }
+
+        public static DataTable ToDataTable<T>(this IEnumerable<T> items)
+        {
+            var tb = new DataTable(typeof(T).Name);
+
+            PropertyInfo[] props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (PropertyInfo prop in props)
+            {
+                Type t = GetCoreType(prop.PropertyType);
+                tb.Columns.Add(prop.Name, t);
+            }
+
+
+            foreach (T item in items)
+            {
+                var values = new object[props.Length];
+
+                for (int i = 0; i < props.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item, null);
+                }
+
+                tb.Rows.Add(values);
+            }
+
+            return tb;
+        }
+
+        public static string IEnumerableToXML<T>(this IEnumerable<T> items)
+        {
+            var tb = new DataTable(typeof(T).Name);
+
+            PropertyInfo[] props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (PropertyInfo prop in props)
+            {
+                Type t = GetCoreType(prop.PropertyType);
+                tb.Columns.Add(prop.Name, t);
+            }
+
+
+            foreach (T item in items)
+            {
+                var values = new object[props.Length];
+
+                for (int i = 0; i < props.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item, null);
+                }
+
+                tb.Rows.Add(values);
+            }
+
+            MemoryStream str = new MemoryStream();
+            tb.WriteXml(str, true);
+            str.Seek(0, SeekOrigin.Begin);
+            StreamReader sr = new StreamReader(str);
+            string xmlstr;
+            xmlstr = sr.ReadToEnd();
+            return (xmlstr);
+        }
+
+        public static List<T> ToNonNullList<T>(this IEnumerable<T> obj)
+        {
+
+            return obj == null ? new List<T>() : obj.ToList();
+        }
+
+        /// <summary>
+        /// Determine of specified type is nullable
+        /// </summary>
+        public static bool IsNullable(Type t)
+        {
+            return !t.IsValueType || (t.IsGenericType && t.GetGenericTypeDefinition() == typeof(Nullable<>));
+        }
+
+        /// <summary>
+        /// Return underlying type if type is Nullable otherwise return the type
+        /// </summary>
+        public static Type GetCoreType(Type t)
+        {
+            if (t != null && IsNullable(t))
+            {
+                if (!t.IsValueType)
+                {
+                    return t;
+                }
+                else
+                {
+                    return Nullable.GetUnderlyingType(t);
+                }
+            }
+            else
+            {
+                return t;
+            }
+        }
+
+       
+    }
+}
--- LiveHolidayapp/LiveHolidayapp/wwwroot/js/theme.js
+++ LiveHolidayapp/LiveHolidayapp/wwwroot/js/theme.js
@@ -819,3 +819,16 @@
     
     
 })(jQuery)
+
+
+// <!-- Loader Script -->
+$(document).ready(function () {
+    window.setTimeout('fadeout();', 1000);
+
+});
+
+function fadeout() {
+    $('#loader').delay(1000).fadeOut('slow', function () {
+        $('.notLoaded').removeClass('notLoaded');
+    });
+}
